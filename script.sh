af67c6affcf3f5903ad02ead2303eb73007b3fcd
aibacgit <168973675+aibacgit@users.noreply.github.com>
2024-11-25 12:00:49 -0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -236,12 +236,12 @@ function provisioning_download() {
 
 provisioning_start
 
-check="/workspace/ComfyUI/models/checkpoints/aZovyaPhotoreal_v3.safetensors"
+check="/workspace/ComfyUI/models/checkpoints/aZovyaPhotoreal.safetensors"
 depth="/workspace/ComfyUI/models/controlnet/controlnet11Models_depth.safetensors"
 canny="/workspace/ComfyUI/models/controlnet/controlnet11Models_canny.safetensors"
 
 if [ ! -f "$check" ]; then
-    wget "https://civitai.com/api/download/models/813603?token=dcd42f9284445bfa79efa7a427bb77a0" -O "$check" --progress=bar
+    wget "https://civitai.com/api/download/models/474400?token=dcd42f9284445bfa79efa7a427bb77a0" -O "$check" --progress=bar
 else
     echo "File already exists."
 fi
